7d2ca5be10634cacd8bc57e344172353e044a9ab
Truong Tan Phuc <phuccan0800@gmail.com>
2023-07-15 09:44:25 +0700

    New Stack2 with ArrayList

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,26 @@
 import  implementations.*;
+
+import java.util.Iterator;
+
 public class Main {
     public static void main(String[] args) {
-
+        testStack();
+    }
+    public static void testStack(){
+        Stack<Integer> stack = new Stack<>();
+        System.out.print("Push + Peek: ");
+        for(int i = 0;i<=10; i++){
+            stack.push(i);
+            System.out.print(stack.peek()+" ");
+        }
+        System.out.println();
+        System.out.println("Size: "+ stack.size());
+        System.out.println("Pop: "+stack.pop());
+        Iterator<Integer> iterator = stack.iterator();
+        while (iterator.hasNext()){
+            System.out.print(iterator.next()+" ");
+        }
+        System.out.println();
+        System.out.println("Empty:"+stack.isEmpty() );
     }
 }
---
+++ src/main/java/implementations/Stack2.java
@@ -0,0 +1,62 @@
+package implementations;
+
+import interfaces.AbstractStack;
+
+import java.util.Iterator;
+
+public class Stack2<E> implements AbstractStack<E> {
+    private ArrayList<E> stack2;
+    private int size;
+
+    public Stack2() {
+        this.stack2 = new ArrayList<>();
+        this.size = 0;
+    }
+
+    @Override
+    public void push(E element){
+        stack2.add(element);
+        this.size++;
+    }
+
+    @Override
+    public E pop() {
+        ensureNonEmpty();
+        E element = stack2.remove(stack2.size()-1);
+        this.size--;
+        return element;
+    }
+    private void ensureNonEmpty(){
+        if (stack2.isEmpty()){
+            throw new IllegalStateException("Stack is empty!!");
+        }
+    }
+    @Override
+    public E peek() {
+        ensureNonEmpty();
+        return stack2.get(stack2.size() - 1);
+    }
+
+    @Override
+    public int size() {
+        return this.size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            private ArrayList<E> current = stack2;
+            @Override
+            public boolean hasNext(){ return current != null; }
+            @Override
+            public E next() {
+                return  current.get(current.size()-1);
+            }
+        };
+    }
+}
